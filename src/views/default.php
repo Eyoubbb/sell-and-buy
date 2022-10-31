1106f5b69707aaeb75c45bf0e8cf40df062293e8
Eviive <albert.vaillon21@gmail.com>
2022-10-31 18:31:51 +0100

    Added fonts and some basic mixins

--- src/views/default.php
+++ src/views/default.php
@@ -4,6 +4,11 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Open+Sans:wght@400;700&family=Source+Code+Pro:wght@400;700&display=swap" rel="stylesheet">
+
 	<?php
 		if (isset($data['stylesheets'])) {
 			foreach ($data['stylesheets'] as $stylesheet) {
@@ -12,6 +17,7 @@
 			}
 		}
 	?>
+
 	<title>Sell &amp; Buy<?= isset($data['title']) ? ' - '.$data['title'] : '' ?></title>
 </head>
 <body>
