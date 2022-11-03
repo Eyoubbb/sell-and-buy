a5598198294694b79351e6dc009de90923399d99
Hazim ASRI <wan-muhammad-hazim.wan-mohd-asri@etu.univ-lyon1.fr>
2022-11-03 12:50:32 +0100

    Add structure of the header

--- src/views/components/header.php
+++ src/views/components/header.php
@@ -1,1 +1,36 @@
-<header>Header</header>
+
+
+<div class="topnav">
+    <!- Left side -->
+    <ul>
+        <li>
+            <a href="#"><img src="/images/logo.svg" alt="" srcset=""></a>
+        </li>
+    </ul>
+    <ul class="nav-item">
+        <li>
+            <a class="nav-item" href="">Acheter</a>
+        </li>
+        <li>
+            <a class="nav-item" href="">Pages</a>
+        </li>
+        <li>
+            <a class="nav-item" href="">Collections</a>
+        </li>
+    </ul>
+        <!-- right -->
+    <ul class="nav-item left">
+        <li>
+            <a class="nav-item" href="">FR</a>
+        </li>
+        <li>
+            <a class="nav-item" href="">EN</a>
+        </li>
+        <li>
+            <a class="nav-item" href="">Se Connecter</a>
+        </li>
+        <li>
+            <a class="nav-item" href="">Rechercher</a>
+        </li>
+    </ul>
+</div>
