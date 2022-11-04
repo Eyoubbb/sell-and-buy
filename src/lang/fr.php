4386f367cf9f28211dbb1dac65e2bdf3e0f16a82
Eviive <albert.vaillon21@gmail.com>
2022-11-05 00:54:20 +0100

    Added both translations

--- src/lang/fr.php
+++ src/lang/fr.php
@@ -5,6 +5,46 @@
 const HOME_WINDOW_TITLE = "Accueil";
 const HOME_TITLE = "Bienvenue sur la page d'Accueil";
 
+/*************** Login & register ***************/
+
+const LOGIN_REGISTER_BACK_TO = "Retourner à l'";
+const LOGIN_REGISTER_HOME = "accueil";
+const LOGIN_REGISTER_PRESENTATION = "Une platforme commerciale pour les creators. Lorem ipsum dolor sit amet. Morbi a bibendum metus. Donec scelerisque sollicitudin enim eu venenatis";
+const LOGIN_REGISTER_ALT_PRESENTATION = "Une illustration présentant Sell & Buy";
+const LOGIN_REGISTER_EMAIL = "Email";
+const LOGIN_REGISTER_PASSWORD = "Mot de passe";
+
+/*************** Login ***************/
+
+const LOGIN_WINDOW_TITLE = "Connexion";
+
+const LOGIN_TITLE = "Se connecter";
+const LOGIN_NEW = "Nouveau chez Sell & Buy ?";
+const LOGIN_SIGNUP = "S'inscrire";
+const LOGIN_EMAIL_ERROR = "Email inconnue";
+const LOGIN_PASSWORD_ERROR = "Mot de passe invalide";
+const LOGIN_SUBMIT = "Se connecter";
+
+/*************** Register ***************/
+
+const REGISTER_WINDOW_TITLE = "Inscription";
+
+const REGISTER_TITLE = "S'inscrire";
+const REGISTER_EXISTING = "Déjà un compte ?";
+const REGISTER_LOGIN = "Se connecter";
+const REGISTER_STEP_ACCOUNT_SETUP = "Configuration du compte";
+const REGISTER_STEP_PERSONAL_DETAILS = "Informations personnelles";
+const REGISTER_STEP_PROFILE_PICTURE = "Photo de profil";
+const REGISTER_PLACEHOLDER_PASSWORD = "8-20 caractères, 1 lettre, 1 chiffre";
+const REGISTER_CONFIRM_PASSWORD = "Confirmer le mot de passe";
+const REGISTER_FIRST_NAME = "Prénom";
+const REGISTER_LAST_NAME = "Nom";
+const REGISTER_PROFILE_PICTURE = "Photo de profil";
+const REGISTER_PROFILE_PICTURE_PREVIEW = "Aperçu de votre photo de profil";
+const REGISTER_NEXT = "Suivant";
+const REGISTER_PREVIOUS = "Précédent";
+const REGISTER_SUBMIT = "S'inscrire";
+
 /*************** Errors ***************/
 
 const ERROR_DEFAULT_WINDOW_TITLE = "Erreur";
@@ -12,3 +52,7 @@ const ERROR_DEFAULT_MESSAGE = "Une erreur est survenue";
 
 const ERROR_404_WINDOW_TITLE = "Erreur 404";
 const ERROR_404_MESSAGE = "La page demandée n'existe pas";
+
+/*************** Images ***************/
+
+const ALT_LOGO = "Le logo de Sell & Buy";
