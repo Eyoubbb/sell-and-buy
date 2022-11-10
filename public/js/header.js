af865971fc3e8bff8628d1508e2a2785aaf5d138
Hazim ASRI <wan-muhammad-hazim.wan-mohd-asri@etu.univ-lyon1.fr>
2022-11-10 10:14:44 +0100

    Refining animation, add timeout to header

--- public/js/header.js
+++ public/js/header.js
@@ -2,6 +2,7 @@
 
     const search = document.getElementById('search');
     const rightBox = document.querySelector('ul.right');
+    let timeout = false
 
     const growSearchBar = () => {
         const form = document.querySelector('form');
@@ -11,10 +12,18 @@
             rightBox.classList.toggle('shift-left');
             rightBox.classList.toggle('shift-right');
         }
+        // form.classList.toggle('hide');
         
-        form.classList.toggle('hide');
+        if(timeout){
+            setTimeout(function() {
+                form.classList.toggle('hide');
+            }, 300);
+            timeout = false;
+        } else{
+            form.classList.toggle('hide');
+            timeout = true;
+        }
 
-    
 
     }
 
--- src/views/components/header.php
+++ src/views/components/header.php
@@ -1,46 +1,46 @@
 <header class="topnav">
     <!- Left side -->
-    <ul>
-        <li>
+    <div>
+        <ul class="nav-item">
             <a href="#"><img src="/images/logo.svg" alt="" srcset=""></a>
-        </li>
-    </ul>
-    <ul class="nav-item">
-        <li>
-            <?= '<a class="hover-link" href="">' . NAV_SHOP . '</a>' ?>
-        </li>
-        <li>
-            <?= '<a class="hover-link" href="">' . NAV_PAGES . '</a>' ?>  
-        </li>
-        <li>
-            <?= '<a class="hover-link" href="">' . NAV_COLLECTIONS . '</a>' ?>
-        </li>
-        <li>
-            <?= '<a class="hover-link" href="">' . NAV_CREATOR . '</a>' ?>
-        </li>
-    </ul>
-        <!-- right -->
-    <ul class="nav-item right">
-        <li>
-            <a class="hover-link" href="">FR</a>
-        </li>
-        <li>
-            <a class="hover-link" href="">EN</a>
-        </li>
-        <li>
-            <?= '<a class="hover-link" href="">' . NAV_LOGIN . '</a>' ?>
-        </li>
-        <li>
-            <?= '<a id="search">' . NAV_SEARCH . '</a>' ?>
-        </li>
-        <li>
-            <form class="hide" method="get">
-                <div class="search-form">
-                    <input  id="text-input" type="text" name="" id="" placeholder="Search..." aria-label="search" required>
-                    <input type="submit" value="OK">
-                </div>
-            </form>
-        </li>
-    
-    </ul>
+            <li>
+                <?= '<a class="hover-link" href="">' . NAV_SHOP . '</a>' ?>
+            </li>
+            <li>
+                <?= '<a class="hover-link" href="">' . NAV_PAGES . '</a>' ?>  
+            </li>
+            <li>
+                <?= '<a class="hover-link" href="">' . NAV_COLLECTIONS . '</a>' ?>
+            </li>
+            <li>
+                <?= '<a class="hover-link" href="">' . NAV_CREATOR . '</a>' ?>
+            </li>
+        </ul>
+    </div>
+    <!-- right -->
+    <div>
+        <ul class="nav-item right">
+            <li>
+                <a class="hover-link" href="">FR</a>
+            </li>
+            <li>
+                <a class="hover-link" href="">EN</a>
+            </li>
+            <li>
+                <?= '<a class="hover-link" href="">' . NAV_LOGIN . '</a>' ?>
+            </li>
+            <li>
+                <?= '<a id="search">' . NAV_SEARCH . '</a>' ?>
+            </li>
+            <li>
+                <form class="hide" method="get">
+                    <div class="search-form">
+                        <input  id="text-input" type="text" name="" id="" placeholder="Search..." aria-label="search" required>
+                        <input type="submit" value="OK">
+                    </div>
+                </form>
+            </li>
+        </ul>
+    </div>
+
 </header>
