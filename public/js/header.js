16af3ddc40d3c2e07b2b6ff3c6a834d40f8a287a
Eviive <albert.vaillon21@gmail.com>
2022-11-15 17:14:30 +0100

    Added a menu for the links

--- public/js/header.js
+++ public/js/header.js
@@ -1,37 +1,95 @@
 (() => {
-
+	
 	/*************** Menu ***************/
 
-	const menuBtn = document.querySelector('.btn-caret');
-	const menu = document.querySelector('.dropdown');
-	
-	const removeEvents = () => {
-		document.removeEventListener('click', closeMenu);
-		menu.removeEventListener('click', stopPropagation);
-		document.removeEventListener('keydown', escapeMenu);
-	};
+	const profileBtn = document.querySelector('.btn-caret');
+	const profileMenu = document.querySelector('.dropdown-profile');
 
-	const stopPropagation = e => e.stopPropagation();
-	
-	const closeMenu = () => {
-		menu.classList.remove('menu-open');
-		removeEvents();
+	const removeProfileEvents = () => {
+		document.removeEventListener('click', closeProfileMenu);
+		profileMenu.removeEventListener('click', stopProfilePropagation);
+		document.removeEventListener('keydown', escapeProfileMenu);
 	};
 	
-	const escapeMenu = e => e.key === 'Escape' && closeMenu();
+	const stopProfilePropagation = e => e.stopPropagation();
 	
-	menuBtn.addEventListener('click', e => {
-		const isOpen = menu.classList.toggle('menu-open');
-		
-		if (isOpen) {
-			e.stopPropagation();
+	const closeProfileMenu = () => {
+		profileMenu.classList.remove('open');
+		removeProfileEvents();
+	};
+	
+	const escapeProfileMenu = e => e.key === 'Escape' && closeProfileMenu();
 
-			document.addEventListener('click', closeMenu);
-			menu.addEventListener('click', stopPropagation);
-			document.addEventListener('keydown', escapeMenu);
-		} else {
-			removeEvents();
+	/*************** Links ***************/
+	
+	const linksBtn = document.querySelector('.btn-links');
+	const linksMenu = document.querySelector('.dropdown-links');
+	
+	const removeLinksEvents = () => {
+		document.removeEventListener('click', closeLinksMenu);
+		linksMenu.removeEventListener('click', stopLinksPropagation);
+		document.removeEventListener('keydown', escapeLinksMenu);
+	};
+	
+	const stopLinksPropagation = e => e.stopPropagation();
+	
+	const closeLinksMenu = () => {
+		linksMenu.classList.remove('open');
+		removeLinksEvents();
+	};
+	
+	const escapeLinksMenu = e => e.key === 'Escape' && closeLinksMenu();
+	
+	/*************** Dropdowns ***************/
+
+	const dropdowns = [
+		{
+			btn: profileBtn,
+			menu: profileMenu,
+			callbacks: {
+				closeMenu: closeProfileMenu,
+				stopPropagation: stopProfilePropagation,
+				escapeMenu: escapeProfileMenu,
+				removeEvents: removeProfileEvents
+			}
+		},
+		{
+			btn: linksBtn,
+			menu: linksMenu,
+			callbacks: {
+				closeMenu: closeLinksMenu,
+				stopPropagation: stopLinksPropagation,
+				escapeMenu: escapeLinksMenu,
+				removeEvents: removeLinksEvents
+			}
 		}
-	});
+	];
+
+	const openDropdown = dropdown => {
+		dropdown.menu.classList.add('open');
+		document.addEventListener('click', dropdown.callbacks.closeMenu);
+		dropdown.menu.addEventListener('click', dropdown.callbacks.stopPropagation);
+		document.addEventListener('keydown', dropdown.callbacks.escapeMenu);
+	};
+
+	const closeDropdown = dropdown => {
+		dropdown.menu.classList.remove('open');
+		dropdown.callbacks.removeEvents();
+	};
+
+	const toggleDropdown = dropdown => {
+		if (dropdown.menu.classList.contains('open')) {
+			closeDropdown(dropdown);
+		} else {
+			for (const d of dropdowns) {
+				d.menu.classList.contains('open') && closeDropdown(d);
+			}
+			openDropdown(dropdown);
+		}
+	};
+
+	for (const d of dropdowns) {
+		d.btn.addEventListener('click', () => toggleDropdown(d));
+	}
 	
 })();
--- src/views/components/header.php
+++ src/views/components/header.php
@@ -1,11 +1,19 @@
 <?php
-	$frUrl = '/fr' . $_SERVER['REQUEST_URI'];
-	$enUrl = '/en' . $_SERVER['REQUEST_URI'];
+	$frUrl = '/fr' . $data['url'];
+	$enUrl = '/en' . $data['url'];
 	$loginUrl = $data['routes']['GET:User#login']->getUrl();
+	$registerUrl = $data['routes']['GET:User#register']->getUrl();
 	$logoutUrl = $data['routes']['GET:User#logout']->getUrl();
 	$searchUrl = $data['routes']['GET:Home#index']->getUrl();
+	$settingsUrl = $data['routes']['GET:Home#index']->getUrl();
+
+	if (isLoggedIn()) {
+		$user = unserialize($_SESSION['user']);
+	} else {
+		$user = null;
+	}
 ?>
-<header class="topnav">
+<header>
 	<!-- left -->
 	<div class="nav-left">
 		<a class="logo" href="<?= $data['routes']['GET:Home#index']->getUrl() ?>">
@@ -13,7 +21,7 @@
 		</a>
 		<ul class="nav-links">
 			<li>
-				<a style="font-weight: bold;"class="hover-link" href="<?= $data['routes']['GET:Home#index']->getUrl() ?>"><?= NAV_SHOP ?></a>
+				<a style="font-weight: bold;" class="hover-link" href="<?= $data['routes']['GET:Home#index']->getUrl() ?>"><?= NAV_SHOP ?></a>
 			</li>
 			<li>
 				<a class="hover-link" href="<?= $data['routes']['GET:Home#index']->getUrl() ?>"><?= NAV_PAGES ?></a>
@@ -26,7 +34,7 @@
 			</li>
 		</ul>
 		<form class="search-form" method="GET" action="<?= $searchUrl ?>">
-			<input name="search" placeholder="<?= NAV_SEARCH_PLACEHOLDER ?>" />
+			<input name="q" placeholder="<?= NAV_SEARCH_PLACEHOLDER ?>" />
 			<button class="btn-search">
 				<img src="<?= PATH_IMAGES . 'search.svg' ?>" alt="<?= ALT_SEARCH ?>" loading="lazy" />
 			</button>
@@ -34,44 +42,96 @@
 	</div>
 	<!-- right -->
 	<div class="nav-right">
-		<a class="btn-search" href="<?= $searchUrl ?>">
+		<a class="btn-icon btn-search" href="<?= $searchUrl ?>">
 			<img src="<?= PATH_IMAGES . 'search.svg' ?>" alt="<?= ALT_SEARCH ?>" loading="lazy" />
 		</a>
-		<div class="dropdown">
+		<div class="dropdown dropdown-links">
+			<button class="btn-icon btn-links">
+				<img src="<?= PATH_IMAGES . 'menu.svg' ?>" alt="<?= ALT_MENU ?>" loading="lazy" />
+			</button>
+			<ul class="dropdown-content">
+				<li>
+					<a style="font-weight: bold;" href="<?= $data['routes']['GET:Home#index']->getUrl() ?>"><?= NAV_SHOP ?></a>
+				</li>
+				<li>
+					<a href="<?= $data['routes']['GET:Home#index']->getUrl() ?>"><?= NAV_PAGES ?></a>
+				</li>
+				<li>
+					<a href="<?= $data['routes']['GET:Home#index']->getUrl() ?>"><?= NAV_COLLECTIONS ?></a>
+				</li>
+				<li>
+					<a href="<?= $data['routes']['GET:Home#index']->getUrl() ?>"><?= NAV_CREATOR ?></a>
+				</li>
+			</ul>
+		</div>
+		<div class="dropdown dropdown-profile">
 			<button class="btn-caret">
 				<?php
-					if (isLoggedIn()) {
-						$path = PATH_USERS . unserialize($_SESSION['user'])->getPictureUrl();
+					if ($user) {
+						$path = PATH_USERS . $user->getPictureUrl();
 						$alt = ALT_PROFILE_PICTURE;
 						echo <<<HTML
 							<img class="profile-picture" src="$path" alt="$alt" loading="lazy" />
 						HTML;
 					} else {
-						echo 'Se connecter';
+						echo NAV_ACCOUNT;
 					}
 				?>
 				<img src="<?= PATH_IMAGES . 'caret-down.svg' ?>" alt="<?= ALT_MENU ?>" loading="lazy" />
 			</button>
 			<ul class="dropdown-content">
+				<?php
+					if ($user) {
+						$settingsText = NAV_ACCOUNT_SETTINGS;
+						echo <<<HTML
+							<li class="profile">
+								<a href="$logoutUrl">
+									<span class="name">{$user->getFirstName()} {$user->getLastName()}</span>
+									<span class="email">{$user->getEmail()}</span>
+								</a>
+							</li>
+							<li class="separator">
+								<hr />
+							</li>
+							<li>
+								<a href="$settingsUrl">$settingsText</a>
+							</li>
+						HTML;
+					} else {
+						$loginText = NAV_LOGIN;
+						$registerText = NAV_REGISTER;
+						echo <<<HTML
+							<li>
+								<a href="$loginUrl">$loginText</a>
+							</li>
+							<li>
+								<a href="$registerUrl">$registerText</a>
+							</li>
+						HTML;
+					}
+				?>
+				<li class="separator">
+					<hr />
+				</li>
 				<li>
 					<a href="<?= $enUrl ?>"><?= EN ?></a>
 				</li>
 				<li>
 					<a href="<?= $frUrl ?>"><?= FR ?></a>
 				</li>
+				<li class="separator">
+					<hr />
+				</li>
 				<?php
-					if (isLoggedIn()) {
+					if ($user) {
 						$text = NAV_LOGOUT;
 						$url = $logoutUrl;
-					} else {
-						$text = NAV_LOGIN;
-						$url = $loginUrl;
+						echo <<<HTML
+							<li>
+								<a href="$url">$text</a>
+							</li>
+						HTML;
 					}
-					echo <<<HTML
-						<li>
-							<a href="$url">$text</a>
-						</li>
-					HTML;
 				?>
 			</ul>
 		</div>
