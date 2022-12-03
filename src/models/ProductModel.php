f8f26e76ab442cbbcce9833b7f3efd3e5cf86f47
Eviive <albert.vaillon21@gmail.com>
2022-12-04 00:08:33 +0100

    Model and query to fetch products

--- src/models/ProductModel.php
+++ src/models/ProductModel.php
@@ -1,13 +1,43 @@
 <?php
 
 require_once PATH_CORE . 'Model.php';
+require_once PATH_ENTITIES . 'Product.php';
+require_once PATH_ENTITIES . 'Category.php';
+require_once PATH_ENTITIES . 'User.php';
 
 class ProductModel extends Model {
 
-	public function getAllProducts() {
+	public function home(): array | false {
 		$productDAO = $this->dao('Product');
 
-		return $productDAO->getAll();
+		$res = $productDAO->getAllProducts();
+
+		if (!$res) {
+			$this->setError('ERROR_FETCHING_PRODUCTS');
+			return false;
+		}
+		
+		$products = [];
+		$categories = [];
+		$creators = [];
+
+		foreach ($res as $row) {
+			$products[] = new Product($row);
+			
+			if (!isset($categories[$row['category_id']])) {
+				$categories[$row['category_id']] = new Category($row);
+			}
+			
+			if (!isset($creators[$row['user_id']])) {
+				$creators[$row['user_id']] = new User($row);
+			}
+		}
+		
+		return [
+			'products' => $products,
+			'categories' => $categories,
+			'creators' => $creators
+		];
 	}
 
 }
--- src/models/dao/ProductDAO.php
+++ src/models/dao/ProductDAO.php
@@ -8,5 +8,18 @@ class ProductDAO extends DAO {
 	public function __construct() {
 		parent::__construct('products');
 	}
+
+	public function getAllProducts() {
+		$sql = 'SELECT *
+				FROM products P
+				JOIN categories C
+					USING (category_id)
+				JOIN users U
+					ON (P.creator_id = U.user_id)
+				WHERE P.product_visible = 1
+				ORDER BY P.product_id';
+
+		return $this->queryAll($sql, null, false);
+	}
 	
 }
