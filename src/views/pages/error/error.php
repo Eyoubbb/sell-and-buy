d69ae648ff72ef01e035175054fe98a4cde4e696
Hazim ASRI <wan-muhammad-hazim.wan-mohd-asri@etu.univ-lyon1.fr>
2022-12-07 19:33:52 +0100

    Add structure of error page

--- src/views/pages/error/error.php
+++ src/views/pages/error/error.php
@@ -1,1 +1,8 @@
-<h2><?= $data['message'] ?></h2>
+<section class="error">
+	<div class="wrapper">
+		<h2><?= $data['title'] ?></h2>
+		<p><?= $data['message'] ?></p>
+		<span><?= $data['code'] ?></span>
+	</div>
+	<img src="<?= PATH_IMAGES . 'error.svg'?>" alt="error">
+</section>
