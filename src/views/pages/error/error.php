db59c534de482acb74e67e8d70091c7316be5382
Hazim ASRI <wan-muhammad-hazim.wan-mohd-asri@etu.univ-lyon1.fr>
2022-12-09 10:35:42 +0100

    Add error message and end of page design

--- src/views/pages/error/error.php
+++ src/views/pages/error/error.php
@@ -1,8 +1,8 @@
 <section class="error">
 	<div class="wrapper">
-		<h2><?= $data['title'] ?></h2>
+		<h2><?= $data['title'] . '. ' ?><span class="extras"><?= ERROR_DEFAULT_WINDOW_TITLE ?></span></h2>
 		<p><?= $data['message'] ?></p>
-		<span><?= $data['code'] ?></span>
+		<span class="extras"><?= ERROR_EXTRAS ?></span>
 	</div>
 	<img src="<?= PATH_IMAGES . 'error.svg'?>" alt="error">
 </section>
