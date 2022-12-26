750367587688d746e4fc1a8a5c0fad1faad89472
Hazim ASRI <wan-muhammad-hazim.wan-mohd-asri@etu.univ-lyon1.fr>
2022-12-26 17:50:45 +0100

    Redesign forms

--- src/views/components/forms/form-profile.php
+++ src/views/components/forms/form-profile.php
@@ -1,11 +1,19 @@
-<label for="name"><?= FORM_NAME ?></label><br>
-<input type="text" id="title" name="title" placeholder="<?= FORM_TITLE_PLACEHOLDER ?>"><br>
-<label for="bio"><?= FORM_BIO ?></label><br>
-<textarea type="text" id="bio" name="bio" placeholder="<?= FORM_PRICE_PLACEHOLDER ?>"></textarea><br>
-<label for="twitter"><?= FORM_TWITTER ?></label>
-<input type="text" id="title" name="title" placeholder="<?= FORM_TITLE_PLACEHOLDER ?>"><br>
-<label for="instagram"><?= FORM_INSTAGRAM ?></label>
-<input type="text" id="title" name="title" placeholder="<?= FORM_TITLE_PLACEHOLDER ?>"><br>
-<label for="instagram"><?= FORM_LINKEDIN ?></label>
-<input type="text" id="title" name="title" placeholder="<?= FORM_TITLE_PLACEHOLDER ?>"><br>
+<label for="name"><?= FORM_NAME ?></label>
+<input type="text" id="title" name="title" placeholder="<?= FORM_TITLE_PLACEHOLDER ?>">
+
+<label for="bio"><?= FORM_BIO ?></label>
+<textarea type="text" id="bio" name="bio" placeholder="<?= FORM_PRICE_PLACEHOLDER ?>"></textarea>
+
+<div class="input-wrapper">
+	<label for="twitter"><?= FORM_TWITTER ?></label>
+	<input type="text" class="social-media" name="twitter" placeholder="<?= FORM_TITLE_PLACEHOLDER ?>">
+</div>
+<div class="input-wrapper">
+	<label for="instagram"><?= FORM_INSTAGRAM ?></label>
+	<input type="text" class="social-media" name="instagram" placeholder="<?= FORM_TITLE_PLACEHOLDER ?>">
+</div>
+<div class="input-wrapper">
+	<label for="linkedin"><?= FORM_LINKEDIN ?></label>
+	<input type="text" class="social-media" name="linkedin" placeholder="<?= FORM_TITLE_PLACEHOLDER ?>">
+</div>
 <input type="submit" value="Update">
